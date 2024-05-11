b73f0dab1cefe9e41d70c525af45c9959b844289
YasmineYaap <jaz.yaap@gmail.com>
2024-05-11 14:56:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,15 @@ def main():
     r = request.form.get("q")
     return(render_template("main.html", r=r))
 
+@app.route("/prediction", methods = ["GET" , "POST"])
+def prediction():
+    return(render_template("prediction.html"))
+
+@app.route("/dbs_price", methods = ["GET" , "POST"])
+def dbs_price():
+    q = float(request.form.get("q"))
+    return(render_template("dbs_price.html", r=(q*-50.6)+90.2))
+
 # executing / launching app
 if __name__ == "__main__":
     app.run()
