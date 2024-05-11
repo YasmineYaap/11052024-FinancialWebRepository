d9a26cf65ea3d2787f33500804c191356697f2f0
YasmineYaap <jaz.yaap@gmail.com>
2024-05-11 13:50:20 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask (__name__)
+
+@app.route("/", methods = ["GET" , "POST"])
+def index():
+    return(render_template("index.html"))
+
+@app.route("/main", methods = ["GET" , "POST"])
+def main():
+    r = request.form.get("q")
+    return(render_template("main.html", r=r))
+
+# executing / launching app
+if __name__ == "__main__":
+    app.run()
