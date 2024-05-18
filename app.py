0a3784e2e1f8427fefea7b6b98dd8847f99ff588
YasmineYaap <jaz.yaap@gmail.com>
2024-05-18 14:27:43 +0800

    Update app day 2.py

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template, request
+from PIL import Image
+import google.generativeai as palm
+import replicate
+import os
+import requests
 
+flag = 1
+name = ""
+palm.configure(api_key="AIzaSyDEM9uvFL2Kb_PkV2rAAohtwiFiKnT8MQE")
+os.environ["REPLICATE_API_TOKEN"] = "api : r8_bQWq15nf3GsVLFOb78twM24oQADAzJN4Q1vA9"
+
+model = {"model" : "models/chat-bison-001"}
 app = Flask (__name__)
 
 @app.route("/", methods = ["GET" , "POST"])
@@ -8,8 +19,12 @@ def index():
 
 @app.route("/main", methods = ["GET" , "POST"])
 def main():
-    r = request.form.get("q")
-    return(render_template("main.html", r=r))
+    global flag, name
+    print("flag", flag)
+    if flag == 1:
+        name = request.form.get("q")
+        flag = 0
+    return(render_template("main.html",r=name))
 
 @app.route("/prediction", methods = ["GET" , "POST"])
 def prediction():
@@ -20,6 +35,33 @@ def dbs_price():
     q = float(request.form.get("q"))
     return(render_template("dbs_price.html", r=(q*-50.6)+90.2))
 
-# executing / launching app
+@app.route("/generate_text", methods = ["GET" , "POST"])
+def generate_text():
+    return(render_template("generate_text.html"))
+
+@app.route("/text_result_makersuite", methods = ["GET" , "POST"])
+def text_result_makersuite():
+    q = request.form.get("q")
+    r = palm.chat(**model, messages=q)
+    return(render_template("text_result_makersuite.html", r=r.last))
+
+@app.route("/generate_image", methods = ["GET" , "POST"])
+def generate_image():
+    return(render_template("generate_image.html"))
+
+@app.route("/image_result", methods = ["GET" , "POST"])
+def image_result():
+    q = request.form.get("q")
+    r = replicate.run("stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+                  input = {"prompt":q}
+    )
+    return(render_template("image_result.html", r=r[0]))
+
+@app.route("/end",methods=["GET","POST"])
+def end():
+    global flag
+    flag = 1
+    return(render_template("index.html"))
+
 if __name__ == "__main__":
     app.run()
